97fd16cab5770251fcdd77d9205b76c6b704b6fe
Costin Manolache <costin@apache.org>
2002-08-17 06:37:06 +0000

    The result of discovery.
    Contains the loader where the descriptor was found, the base URL and
    the name of the service.
    
    ( we assume that each jar may contain one or more services ).
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/discovery/trunk@132809 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/commons/discovery/ServiceInfo.java
@@ -0,0 +1,143 @@
+/*
+ * ====================================================================
+ *
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 1999-2002 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Commons", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+
+package org.apache.commons.discovery;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Properties;
+import java.util.Vector;
+import java.net.URL;
+
+/**
+ * 'Service' located by the discovery.
+ * 
+ * @author Richard A. Sitze
+ * @author Craig R. McClanahan
+ * @author Costin Manolache
+ */
+public class ServiceInfo 
+{
+    String implName;
+    ClassLoader loader;
+    URL location;
+
+    public ServiceInfo() {
+    }
+
+    public ServiceInfo(String implName, ClassLoader loader, URL location) {
+        setImplName( implName );
+        setLoader( loader );
+        setURL( location );
+    }
+
+    /**
+     * Get the value of URL.
+     * @return value of URL.
+     */
+    public URL getURL() {
+        return location;
+    }
+    
+    /**
+     * Set the value of URL.
+     * @param v  Value to assign to URL.
+     */
+    public void setURL(URL  v) {
+        this.location = v;
+    }
+    
+
+    /**
+     * Get the value of implName.
+     * @return value of implName.
+     */
+    public String getImplName() {
+        return implName;
+    }
+    
+    /**
+     * Set the value of implName.
+     * @param v  Value to assign to implName.
+     */
+    public void setImplName(String  v) {
+        this.implName = v;
+    }
+    
+    /**
+     * Get the value of loader.
+     * @return value of loader.
+     */
+    public ClassLoader getLoader() {
+        return loader;
+    }
+    
+    /**
+     * Set the value of loader.
+     * @param v  Value to assign to loader.
+     */
+    public void setLoader(ClassLoader  v) {
+        this.loader = v;
+    }
+    
+    public String toString() {
+        return "Service " + implName + " " + loader + " " + location;
+    }
+
+}
