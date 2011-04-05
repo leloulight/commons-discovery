9fdb6046bbc2606f5ef1acc815dfd451bd39e431
Simone Tripodi <simonetripodi@apache.org>
2011-04-05 13:35:47 +0000

    added missing javadoc
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/discovery/trunk@1089034 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/discovery/tools/PropertiesHolder.java
+++ src/java/org/apache/commons/discovery/tools/PropertiesHolder.java
@@ -31,24 +31,38 @@ public class PropertiesHolder {
     private Properties   properties;
     private final String propertiesFileName;
 
+    /**
+     * Creates a new {@code PropertiesHolder} instance given an
+     * already load {@code Properties} set.
+     *
+     * @param properties The already load {@code Properties} set
+     */
     public PropertiesHolder(Properties properties) {
         this.properties = properties;
         this.propertiesFileName = null;
     }
 
+    /**
+     * Creates a new {@code PropertiesHolder} instance given a
+     * property file name.
+     *
+     * @param propertiesFileName The property file name
+     */
     public PropertiesHolder(String propertiesFileName) {
         this.properties = null;
         this.propertiesFileName = propertiesFileName;
     }
 
     /**
+     * Returns the {@code Properties} instance, loaded if necessary from {@code propertiesFileName}.
+     *
      * @param spi Optional SPI (may be null).
      *            If provided, an attempt is made to load the
      *            property file as-per Class.getResource().
      *
      * @param loaders Used only if properties need to be loaded.
      * 
-     * @return Properties.  Load the properties if necessary.
+     * @return The {@code Properties}, loaded if necessary.
      */
     public Properties getProperties(SPInterface<?> spi, ClassLoaders loaders) {
         if (properties == null) {
@@ -57,6 +71,11 @@ public class PropertiesHolder {
         return properties;
     }
 
+    /**
+     * Returns the property file name
+     *
+     * @return The property file name
+     */
     public String getPropertiesFileName() {
         return propertiesFileName;
     }
