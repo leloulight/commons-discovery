89bdcf448a040874d5d1609e13ad016b6bd99cce
Simone Tripodi <simonetripodi@apache.org>
2011-04-05 05:49:35 +0000

    fixed checkstyle violations
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/discovery/trunk@1088901 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/discovery/ResourceClass.java
+++ src/java/org/apache/commons/discovery/ResourceClass.java
@@ -31,21 +31,43 @@ import org.apache.commons.logging.Log;
  *
  * I think it works well as it will give users a point-of-reference.
  *
+ * @param <T> The SPI type
+ *
  * @author Richard A. Sitze
  */
 public class ResourceClass<T> extends Resource
 {
     private static Log log = DiscoveryLogFactory.newLog(ResourceClass.class);
+
+    /**
+     * Sets the {@code Log} for this class.
+     *
+     * @param _log This class {@code Log}
+     */
     public static void setLog(Log _log) {
         log = _log;
     }
     protected Class<? extends T>       resourceClass;
 
+    /**
+     * Create a new {@code Resource} class located by discovery.
+     *
+     * @param <S> Any type extends T
+     * @param resourceClass The resource class has to be located
+     * @param resource The resource URL has to be located
+     */
     public <S extends T> ResourceClass(Class<S> resourceClass, URL resource) {
         super(resourceClass.getName(), resource, resourceClass.getClassLoader());
         this.resourceClass = resourceClass;
     }
 
+    /**
+     * Create a new {@code Resource} class located by discovery.
+     *
+     * @param resourceName The resource class name has to be located
+     * @param resource The resource URL has to be located
+     * @param loader The class loaders holder
+     */
     public ResourceClass(String resourceName, URL resource, ClassLoader loader) {
         super(resourceName, resource, loader);
     }
@@ -63,6 +85,8 @@ public class ResourceClass<T> extends Resource
      * (wrapping java.lang.NoClassDefFoundError) when calling
      * java.lang.newInstance (JDK13),
      * and who knows what else..
+     *
+     * @param <S> Any type extends T
      *
      * @return value of resourceClass.
      */
@@ -90,6 +114,9 @@ public class ResourceClass<T> extends Resource
         return returned;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String toString() {
         return "ResourceClass[" + getName() +  ", " + getResource() + ", " + getClassLoader() + "]";
